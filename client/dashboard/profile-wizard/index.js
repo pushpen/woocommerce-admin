8e4ce191f50cf7972de38db4cb74150bebff599b
Joshua Flowers <joshuatf@gmail.com>
2019-05-22 15:40:44 +0800

    Add method to update profile params

--- client/dashboard/profile-wizard/index.js
+++ client/dashboard/profile-wizard/index.js
@@ -2,12 +2,15 @@
 /**
  * External dependencies
  */
+import apiFetch from '@wordpress/api-fetch';
 import { Component, createElement } from '@wordpress/element';
+import { compose } from '@wordpress/compose';
+import { withDispatch } from '@wordpress/data';
 
 /**
  * WooCommerce dependencies
  */
-import { updateQueryString } from '@woocommerce/navigation';
+import { stringifyQuery, updateQueryString } from '@woocommerce/navigation';
 
 /**
  * Internal depdencies
@@ -31,10 +34,11 @@ const getSteps = () => {
 	return steps;
 };
 
-export default class ProfileWizard extends Component {
+class ProfileWizard extends Component {
 	constructor() {
 		super( ...arguments );
 		this.goToNextStep = this.goToNextStep.bind( this );
+		this.updateProfile = this.updateProfile.bind( this );
 	}
 
 	componentDidMount() {
@@ -45,10 +49,6 @@ export default class ProfileWizard extends Component {
 	componentWillUnmount() {
 		document.documentElement.classList.add( 'wp-toolbar' );
 		document.body.classList.remove( 'woocommerce-profile-wizard__body' );
-	}
-
-	closeWizard() {
-		// @todo This should close the wizard and mark the profiler as complete via the API.
 	}
 
 	getCurrentStep() {
@@ -68,10 +68,24 @@ export default class ProfileWizard extends Component {
 		const nextStep = getSteps()[ currentStepIndex + 1 ];
 
 		if ( 'undefined' === nextStep ) {
-			return this.closeWizard();
+			this.updateProfile( { complete: true } );
 		}
 
 		return updateQueryString( { step: nextStep.key } );
+	}
+
+	updateProfile( params ) {
+		const { addNotice } = this.props;
+		const payload = stringifyQuery( params );
+
+		return apiFetch( {
+			path: `/wc-admin/v1/onboarding/profile${ payload }`,
+			method: 'POST',
+		} ).catch( error => {
+			if ( error && error.message ) {
+				addNotice( { status: 'error', message: error.message } );
+			}
+		} );
 	}
 
 	render() {
@@ -82,6 +96,17 @@ export default class ProfileWizard extends Component {
 			query,
 			step,
 			goToNextStep: this.goToNextStep,
+			updateProfile: this.updateProfile,
 		} );
 	}
 }
+
+export default compose(
+	withDispatch( dispatch => {
+		const { addNotice } = dispatch( 'wc-admin' );
+
+		return {
+			addNotice,
+		};
+	} )
+)( ProfileWizard );
